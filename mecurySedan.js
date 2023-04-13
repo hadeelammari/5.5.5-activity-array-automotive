f1d5e5e97f4402c08cbf9e2e79ae949527c4d8f7
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-04-12 18:38:55 -0700

    Update mecurySedan.js

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,20 +1,8 @@
-
-// //Mercury boject 
-// let car = new MercuryA28Sedan ("Mecury", "Sedan", "1965", "blue", "0");
-
-// car.loadPassenger(3);
-
-// //start engine 
-// car.start()
-
-// //schedule maintenance 
-
-// car.scheduleService(4000)
-
+const Vehicle = require('./vehicle').Vehicle
 
 class Car extends Vehicle {
     constructor(make, model, year, color, mileage) {
-    super(make, model, year, color, mileage)  
+        super(make, model, year, color, mileage)
         // this.make = "Mercury Sedan";
         // this.model = "A28";
         // this.year = year;
@@ -25,44 +13,45 @@ class Car extends Vehicle {
         this.mileage = 10;
         // this.started = true;
         this.numberOfWheels = 4;
-        this.scheduleService = false;   
+        this.scheduleService = false;
+    }
+
+
+
+    checkService() {
+        if (this.mileage > 3000) {
+            this.scheduleService
+            return this.scheduleService
+        }
+    }
+
+
+    start() {
+        if (this.fuel > 0) {
+            console.log("Engine Started !")
+            return this.started = true;
+        }
+        else {
+            console.log("Engine cannot start...");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger, this.maximumPassengers) {
+            if ((num + this.passenger) <= this.maximumPassengers) {
+                this.passenger = num;
+                return this.passenger;
+            } else {
+                console.log(this.model + " " + this.make + "not have enough space to take all passengers.");
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
     }
 }
 
-
-checkService(); {
-    if (this.mileage > 3000){
-        this.scheduleService
-        return this.scheduleService
-    }
-}
-
-
-start(); {
-    if (this.fuel > 0) {
-    console.log("Engine Started !") 
-    return this.started = true;
-    } 
-    else {
-    console.log("Engine cannot start...");
-    return this.started = false;
-    }
-}
-
-passenger(); {
-if (this.passenger , this.maximumPassengers) {
-    if ((num + this.passenger) <= this.maximumPassengers) {
-        this.passenger = num;
-        return this.passenger;
-    } else {
-        console.log ( this.model + " " + this.make + "not have enough space to take all passengers.");
-    }
-}else{
-    console.log ( this.model + " " + this.make + " is full");
-}
-    }
-
-let myCar = new Car ('mercury', 'A-28_sedan', '2004', 'blue', 50000)
+let myCar = new Car('mercury', 'A-28_sedan', '2004', 'blue', 50000)
 
 myCar.start()
 myCar.loadPassenger(5)
@@ -70,4 +59,3 @@ myCar.stop()
 myCar.checkService()
 
 console.log(myCar)
-
