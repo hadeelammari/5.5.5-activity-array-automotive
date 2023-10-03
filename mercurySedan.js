89c125ea1ebfc7adad0861295a6762e7c7d95c52
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-10-02 22:28:27 -0700

    almost done with prject, realized was havng it in the wrong file

--- mercurySedan.js
+++ mercurySedan.js
@@ -7,6 +7,17 @@ console.log(v.make)
 
 
 //After you write the derived Car class, you should test it out.
+class Car extends VehicleModule {
+    constructor (make, model, year, color, milage) {
+        this.maxPass = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+        super(make, model, year, color, milage);
+    }
+}
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
 
