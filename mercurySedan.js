d3d980344520e13053aef52910af07d5db96c437
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-10-02 22:54:01 -0700

    finished project

--- mercurySedan.js
+++ mercurySedan.js
@@ -7,8 +7,10 @@ console.log(v.make)
 
 
 //After you write the derived Car class, you should test it out.
-class Car extends VehicleModule {
-    constructor (make, model, year, color, milage) {
+class Car extends VehicleModule
+ {
+    constructor (make, model, year, color, milage) 
+    {
         this.maxPass = 5;
         this.passenger = 0;
         this.numberOfWheels = 4;
@@ -19,10 +21,49 @@ class Car extends VehicleModule {
     }
 }
 
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
+maintenance (milage)
+{
+    if(milage > 30000)
+    {
+        this.scheduleService = true;
+        console.log('Time for a checkup');
+    }
+    return this.scheduleService;
+}
 
+begin ()
+{
+    if(fuel > 0)
+    {
+        this.start = true;
+        console.log('Car engine on')
+        return this.start;
+    }
+    else{
+        this.start = false;
+        console.log('Fuel too low');
+        return this.start;
+    }
+}
 
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+loadPassenger (num)
+{
+if(num > 5){
+    console.log('The car is full');
+}
+else{
+    console.log('There is space');
+}
+}
+
+let myCar = new Car ("Mercury", "Sedan", "2005", "green", "60000");
+
+myCar.start()
+myCar.loadPassenger(7)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
 
 
 
