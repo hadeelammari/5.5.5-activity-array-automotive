35c4946520ca42a901dfdc8f315c5eb8ae4f23aa
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-10-04 14:55:45 -0700

    Update mercurySedan.js
    Fixed syntax erros.
    extends was not extending from class
    the super (calling the parent constructor) should be the first line in the child class constructor
    Please review all the changes

--- mercurySedan.js
+++ mercurySedan.js
@@ -7,61 +7,56 @@ console.log(v.make)
 
 
 //After you write the derived Car class, you should test it out.
-class Car extends VehicleModule
- {
-    constructor (make, model, year, color, milage) 
-    {
+class Car extends VehicleModule.Vehicle {
+    constructor(make, model, year, color, milage) {
+        super(make, model, year, color, milage);
         this.maxPass = 5;
         this.passenger = 0;
         this.numberOfWheels = 4;
         this.maxSpeed = 160;
         this.fuel = 10;
         this.scheduleService = false;
-        super(make, model, year, color, milage);
+        
+    }
+
+
+    maintenance(milage) {
+        if (milage > 30000) {
+            this.scheduleService = true;
+            console.log('Time for a checkup');
+        }
+        return this.scheduleService;
+    }
+
+    begin() {
+        if (fuel > 0) {
+            this.start = true;
+            console.log('Car engine on')
+            return this.start;
+        }
+        else {
+            this.start = false;
+            console.log('Fuel too low');
+            return this.start;
+        }
+    }
+
+    loadPassenger(num) {
+        if (num > 5) {
+            console.log('The car is full');
+        }
+        else {
+            console.log('There is space');
+        }
     }
 }
 
-maintenance (milage)
-{
-    if(milage > 30000)
-    {
-        this.scheduleService = true;
-        console.log('Time for a checkup');
-    }
-    return this.scheduleService;
-}
-
-begin ()
-{
-    if(fuel > 0)
-    {
-        this.start = true;
-        console.log('Car engine on')
-        return this.start;
-    }
-    else{
-        this.start = false;
-        console.log('Fuel too low');
-        return this.start;
-    }
-}
-
-loadPassenger (num)
-{
-if(num > 5){
-    console.log('The car is full');
-}
-else{
-    console.log('There is space');
-}
-}
-
-let myCar = new Car ("Mercury", "Sedan", "2005", "green", "60000");
+let myCar = new Car("Mercury", "Sedan", "2005", "green", "60000");
 
 myCar.start()
 myCar.loadPassenger(7)
 myCar.stop()
-myCar.checkService()
+myCar.maintenance()
 
 console.log(myCar)
 
