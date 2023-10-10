39f51422f9d27335b4f46412cceae9daf942bfea
ZvsZig <142462854+ZvsZig@users.noreply.github.com>
2023-10-10 15:58:18 -0700

    finished product

--- mercurySedan.js
+++ mercurySedan.js
@@ -6,30 +6,43 @@ let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage"
 console.log(v.make)
 
 
-//After you write the derived Car class, you should test it out.
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
 
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
+    loadPassenger(num){
+        if(this.passengers > this.maximumPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                console.log("Eh, we'll be fine!")
+                return this.passenger;
+            } 
+        } else{
+            console.log(this.make + ' ' + this.model + 'is at maximum capacity!')
+        }
+    }
 
+    start(){
+        if(this.fuel > 0){
+            console.log("START YOUR ENGINES!!!")
+            return this.started = true;
+        } else {
+            console.log("We're not going anywhere")
+            return this.started = false;
+        }
+    }
 
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
-
-
-
-
-
-
-
-
-
-
-
-
-//TO DO: Creating Instances and Testing Them
-
-//You can use the same instance "v" of the Vehicle class above for the base class.
-
-
-
-
-
-//Create at least two new instances of the Car class and test them here:
+    scheduleService(mileage){
+        if(this.mileage > 30000) {
+            this.scheduleService = true;
+            return this.scheduleService;
+        }
+    }
+}
