81cb7de15119775ab5978847c1d250154ebdfdcb
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-10-11 18:42:09 -0700

    Update mercurySedan.js
    Please review the changes and comments added

--- mercurySedan.js
+++ mercurySedan.js
@@ -6,7 +6,7 @@ let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage"
 console.log(v.make)
 
 
-class Car extends VehicleModule {
+class Car extends VehicleModule.Vehicle {
     constructor(make, model, year, color, mileage){
         super(make, model, year, color, mileage);
         this.maximumPassengers = 5;
@@ -29,20 +29,29 @@ class Car extends VehicleModule {
         }
     }
 
-    start(){
-        if(this.fuel > 0){
-            console.log("START YOUR ENGINES!!!")
-            return this.started = true;
-        } else {
-            console.log("We're not going anywhere")
-            return this.started = false;
-        }
-    }
+    //This function already exists in the parent class
+    // start(){
+    //     if(this.fuel > 0){
+    //         console.log("START YOUR ENGINES!!!")
+    //         return this.started = true;
+    //     } else {
+    //         console.log("We're not going anywhere")
+    //         return this.started = false;
+    //     }
+    // }
 
-    scheduleService(mileage){
+    maintanace (mileage){ //You can not have a property and a method with the same name so I changed the name for the method
         if(this.mileage > 30000) {
             this.scheduleService = true;
             return this.scheduleService;
         }
     }
 }
+
+//You should have some testing code here 
+let sedan = new Car("Jeep", "Cherokee", "2010", "grey");
+console.log(sedan)
+sedan.start();
+sedan.accelerate();
+sedan.maintanace(39000)
+
